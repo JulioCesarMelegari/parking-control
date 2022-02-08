5804e8d083536fe82a6737e2ae8b64631fc73a0e
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2022-02-08 13:41:52 -0300

    Projeto inicial, postgresql, h2

---
+++ src/test/java/com/JCM/parkingcontrol/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JCM.parkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
